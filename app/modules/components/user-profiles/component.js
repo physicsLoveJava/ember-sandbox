323ac06caef2c64fb8cc0a68624c5f5704ca944c
LuJian <lujian282012@163.com>
2016-10-14 00:12:50 +0800

    remove debug

--- app/modules/components/user-profiles/component.js
+++ app/modules/components/user-profiles/component.js
@@ -2,12 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['card'],
-  actions: {
-    select(user) {
-      console.log('select user');
-    },
-    selectUser(user) {
-      console.log('profile', user);
-    }
-  }
 });
--- app/modules/index/controller.js
+++ app/modules/index/controller.js
@@ -1,19 +1,13 @@
 import Controller from 'ember-controller';
-import get from 'ember-metal/get';
-import set from 'ember-metal/set';
 
 export default Controller.extend({
 
   actions: {
-    select(user) {
-      console.log('index select', user);
+    selectUser() {
+
     },
-    selectUser(user) {
-      console.log('index selectUser', user);
-    },
-    clearUser(user) {
-      console.log('index clearUser', user);
+    clearUser() {
+
     }
   }
-
 });
